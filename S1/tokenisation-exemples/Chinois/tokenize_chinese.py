23505aa357ce280ddc11efed0f101c1f59b0f71f
Pierre M <pierre.magistry@univ-amu.fr>
2022-12-07 09:08:20 +0100

    ajout de la commande thulac en commentaire

--- S1/tokenisation-exemples/Chinois/tokenize_chinese.py
+++ S1/tokenisation-exemples/Chinois/tokenize_chinese.py
@@ -2,6 +2,10 @@ import thulac
 import errno
 import fileinput
 
+# autre possibilité, lancer la commande:
+# python -m thulac chinois.txt chinois_seg_output.txt -seg_only
+# Mais ne permet pas les redirections d'entrées/sorties
+
 seg = thulac.thulac(seg_only=True)
 try:
     for line in fileinput.input():
